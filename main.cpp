fd4072ace50257e4d984bdc223b39e55caaa2396
Vitor-Novaes <vitornovaes27@gmail.com>
2018-05-18 00:00:14 -0300

    Modelagem e métodos inits

--- main.cpp
+++ main.cpp
@@ -1,25 +1,74 @@
 #include <iostream>
+#include "models.h"
 using namespace std;
 
-// GrandLine::GrandLine(){}
-// GrandLine::~GrandLine(){}
-// Elemento::Elemento(){}
-// Elemento::~Elemento(){}
-// Pessoa::Pessoa():Elemento(){}
-// Pessoa::~Pessoa(){}
 
+Elemento::Elemento(string nome, TipoElemento tipo){
+    this->nome = nome;
+    this->tipo = tipo;
+}
+Elemento::~Elemento(){}
+//////////////////////////////////////////////////////////
+Pessoa::Pessoa(int hp):Elemento(nome,tipo){
+    this->peso = peso;
+}
+Pessoa::~Pessoa(){}
+//////////////////////////////////////////////////////////
+OnePiece::OnePiece(float peso):Elemento(nome,tipo){
+    this->peso = peso;
+}
+void OnePiece::set_peso(float peso){
+    this->peso = peso
+}
+float OnePiece::get_peso(){
+    return peso;
+}
+OnePiece::~OnePiece(){}
+
+//////////////////////////////////////////////////////////
+Pirata::Pirata(float peso):Pessoa(hp){
+    this->peso = peso;
+    peso_adicional = 0;
+}
+void Pirata::set_peso_adicional(float peso_adicional){
+    this->peso_adicional = peso_adicional;
+}
+float Pirata::get_peso_adicional(){
+    return peso_adicional;
+}
+void Pirata::set_peso(float peso){
+    this->peso = peso;
+}
+float Pirata::get_peso(){
+    return peso;
+}
+Pirata::~Pirata(){}
+
+//////////////////////////////////////////////////////////
+Marinha::Marinha(bool estado):Pessoa(hp){
+    this->estado = estado;
+}
+void Marinha::set_estado(bool estado){
+    this->estado = estado;
+}
+bool Marinha::get_estado(){
+    return estado;
+}
+Marinha::~Marinha(){}
+//////////////////////////////////////////////////////////
+GrandLine::GrandLine(vector<vector<Elemento>> init_cenario){
+    cenario = init_cenario;
+}
+void GrandLine::set_grand_line(vector<vector<Elemento>> cenar){
+    cenario = cenar;
+}
+vector<vector<Elemento>> GrandLine::get_grand_line(){
+    return cenario;
+}
+GrandLine::~GrandLine(){}
 
 
 int main(){
 
-
-
-
-
-
-
-
-
-
-    return 0;
+	return 0;
 }
--- models.h
+++ models.h
@@ -3,58 +3,74 @@
 using namespace std;
 
 class GrandLine {
-private:
-    vector<vector<Elemento>> cenario;
-public:
-    GrandLine(vector<vector<Elemento>> cenar);
-    ~GrandLine();
+    private:
+        vector<vector<Elemento>> cenario;
+    public:
+        GrandLine(vector<vector<Elemento>> cenar);
+        ~GrandLine();
+        void set_grand_line(vector<vector<Elemento>> cenar);
+        vector<vector<Elemento>> get_grand_line();
 };
 
 enum TipoElemento{Pirata,Marinha,EspacoVazio,Obstaculo,OnePiece};
-enum Movimento{up,down,left,right};
+enum Movimento{up = 1,down,left,right};
 
+class Elemento { // abstrata
+    private:
+        string nome;
+        TipoElemento tipo;
+    public:
+        Elemento(string nome, TipoElemento tipo);
+        ~Elemento();
+        virtual void set_tipo_elemento(TipoElemento tipo) = 0;
+        virtual TipoElemento get_tipo_elemento() = 0;
 
-// Classes Abstratas
-class Elemento {
-private:
-    string nome;
-    TipoElemento tipo;
-public:
-    Elemento();
-    ~Elemento();
-};
-
-class Pessoa: public Elemento {
-private:
-    int hp;
-public:
-    Pessoa():Elemento();
-    ~Pessoa();
+        virtual void set_nome_elemento(string nome) = 0;
+        virtual string get_nome_elemento() = 0;
 };
 
 
-// Classes herdeiras
+class Pessoa: public Elemento { // abstrata
+    private:
+        int hp;
+    public:
+        Pessoa(int hp);
+        ~Pessoa();
+        virtual void set_hp(int hp) = 0;
+        virtual int get_hp() = 0;
+};
+
+// RAMBOX ROBLOX
 class OnePiece : public Elemento{
-private:
-    float peso;
-public:
-    OnePiece();
-    ~OnePiece();
+    private:
+        float peso;
+    public:
+        OnePiece(float peso);
+        ~OnePiece();
+        void set_peso(float peso);
+        float get_peso();
 };
 
 class Pirata: public Pessoa {
-private:
-    float peso;
-    float peso_add; // => 0
-public:
-    Pirata();
-    ~Pirata();
+    private:
+        float peso;
+        float peso_adicional; // => 0
+    public:
+        Pirata(float peso);
+        ~Pirata();
+        void set_peso_adicional(float peso_adicional);
+        float get_peso_adicional();
+
+        void set_peso(float peso);
+        float get_peso();
 };
 
 class Marinha: public Pessoa {
-private:
-    bool estado;
-public:
-    Marinha();
-    ~Marinha();
+    private:
+        bool estado;
+    public:
+        Marinha(bool estado);
+        ~Marinha();
+        void set_estado(bool estado);
+        bool get_estado();
 };
